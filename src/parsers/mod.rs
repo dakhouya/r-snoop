e3c51c08fa6ce201100710c94a4cbb9d9a8491e9
David Khouya <dakhouya@gmail.com>
2025-12-28 21:12:24 +0000

    chore: fix clippy warnings

--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -5,13 +5,9 @@ use crate::capture::device_info::DeviceInfo;
 pub fn handle_packet(packet_data: &[u8]) -> Option<DeviceInfo> {
     match PacketHeaders::from_ethernet_slice(packet_data) {
         Ok(headers) => {
-            if let Some(LinkHeader::Ethernet2(eth_header)) = headers.link {
-                match eth_header.ether_type {
-                    EtherType::ARP => {
-                        return arp::handle_arp(packet_data);
-                    },
-                    _ => {}
-                }
+            if let Some(LinkHeader::Ethernet2(eth_header)) = headers.link
+                && eth_header.ether_type == EtherType::ARP {
+                return arp::handle_arp(packet_data);
             }
             None
         }
